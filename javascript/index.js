b6d9b632e7f5f9620eb8cf4322534e0899b13ee8
esG3 <esmeG3@users.noreply.github.com>
2024-02-05 14:37:34 +0000

    edited the css of the homepage, added cities to the drop down and homepage

--- javascript/index.js
+++ javascript/index.js
@@ -1,29 +1,58 @@
-function updateTime(){
-let brisbaneElement = document.querySelector("#brisbane");
-if (brisbaneElement) {
-let brisbaneDateElement = brisbaneElement.querySelector(".date");
-let brisbaneTimeElement = brisbaneElement.querySelector(".time");
-let brisbaneTime = moment().tz("Australia/Brisbane");
+function updateTime() {
+  let brisbaneElement = document.querySelector("#brisbane");
+  if (brisbaneElement) {
+    let brisbaneDateElement = brisbaneElement.querySelector(".date");
+    let brisbaneTimeElement = brisbaneElement.querySelector(".time");
+    let brisbaneTime = moment().tz("Australia/Brisbane");
 
-brisbaneDateElement.innerHTML = brisbaneTime.format("MMMM Do YYYY");
-brisbaneTimeElement.innerHTML = brisbaneTime.format("hh:mm:ss [<small>]A[</small>]");
-}
+    brisbaneDateElement.innerHTML = brisbaneTime.format("MMMM Do YYYY");
+    brisbaneTimeElement.innerHTML = brisbaneTime.format(
+      "hh:mm:ss [<small>]A[</small>]"
+    );
+  }
 
-let stockholmElement = document.querySelector("#stockholm");
-if (stockholmElement) {
-let stockholmDateElement = stockholmElement.querySelector(".date");
-let stockholmTimeElement = stockholmElement.querySelector(".time");
-stockholmTime = moment().tz("Europe/Stockholm");
+  let stockholmElement = document.querySelector("#stockholm");
+  if (stockholmElement) {
+    let stockholmDateElement = stockholmElement.querySelector(".date");
+    let stockholmTimeElement = stockholmElement.querySelector(".time");
+    stockholmTime = moment().tz("Europe/Stockholm");
 
-stockholmDateElement.innerHTML = stockholmTime.format("MMMM Do YYYY");
-stockholmTimeElement.innerHTML = stockholmTime.format("hh:mm:ss [<small>]A[</small>]");
-}
+    stockholmDateElement.innerHTML = stockholmTime.format("MMMM Do YYYY");
+    stockholmTimeElement.innerHTML = stockholmTime.format(
+      "hh:mm:ss [<small>]A[</small>]"
+    );
+  }
+
+  let dubaiElement = document.querySelector("#dubai");
+  if (dubaiElement) {
+    let dubaiDateElement = dubaiElement.querySelector(".date");
+    let dubaiTimeElement = dubaiElement.querySelector(".time");
+    dubaiTime = moment().tz("Asia/Dubai");
+
+    dubaiDateElement.innerHTML = dubaiTime.format("MMMM Do YYYY");
+    dubaiTimeElement.innerHTML = dubaiTime.format(
+      "hh:mm:ss [<small>]A[</small>]"
+    );
+  }
+
+  let singaporeElement = document.querySelector("#singapore");
+  if (singaporeElement) {
+    let singaporeDateElement = singaporeElement.querySelector(".date");
+    let singaporeTimeElement = singaporeElement.querySelector(".time");
+    singaporeTime = moment().tz("Asia/Singapore");
+
+    singaporeDateElement.innerHTML = singaporeTime.format("MMMM Do YYYY");
+    singaporeTimeElement.innerHTML = singaporeTime.format(
+      "hh:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 function updateCity(event) {
+  function updateCityTime() {
     let cityTimeZone = event.target.value;
-    if (cityTimeZone === "current"){
-        cityTimeZone = moment.tz.guess();
+    if (cityTimeZone === "current") {
+      cityTimeZone = moment.tz.guess();
     }
     let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
@@ -39,15 +68,12 @@ function updateCity(event) {
         )} <small>${cityTime.format("A")}</small> </div>
     </div>
     <div>
-<a href="/">All Cities</a> 
+<a id="homepage" href="/">Back to all contries 🌎</a> 
 </div> 
-<style>
-a {
-    text-decoration:none;
+`;
+  }
+  setInterval(updateCityTime, 1000);
 }
-</style>`;
-}
-
 
 updateTime();
 setInterval(updateTime, 1000);
