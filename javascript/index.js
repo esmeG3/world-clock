7988c16948f823419854eec092d597a2bfdb1fce
esG3 <esmeG3@users.noreply.github.com>
2024-02-05 13:58:15 +0000

    added all cities link

--- javascript/index.js
+++ javascript/index.js
@@ -1,19 +1,23 @@
 function updateTime(){
 let brisbaneElement = document.querySelector("#brisbane");
+if (brisbaneElement) {
 let brisbaneDateElement = brisbaneElement.querySelector(".date");
 let brisbaneTimeElement = brisbaneElement.querySelector(".time");
 let brisbaneTime = moment().tz("Australia/Brisbane");
 
 brisbaneDateElement.innerHTML = brisbaneTime.format("MMMM Do YYYY");
 brisbaneTimeElement.innerHTML = brisbaneTime.format("hh:mm:ss [<small>]A[</small>]");
+}
 
 let stockholmElement = document.querySelector("#stockholm");
+if (stockholmElement) {
 let stockholmDateElement = stockholmElement.querySelector(".date");
 let stockholmTimeElement = stockholmElement.querySelector(".time");
 stockholmTime = moment().tz("Europe/Stockholm");
 
 stockholmDateElement.innerHTML = stockholmTime.format("MMMM Do YYYY");
 stockholmTimeElement.innerHTML = stockholmTime.format("hh:mm:ss [<small>]A[</small>]");
+}
 }
 
 function updateCity(event) {
@@ -30,14 +34,23 @@ function updateCity(event) {
           <h2>${cityName}</h2>
           <div class="date"> ${cityTime.format("MMMM Do YYYY")} </div>
         </div>
-        <div class="time">${cityTime.format("hh:mm:ss")} <small>${cityTime.format("A")}</small> </div>
-      </div>`;
-
+        <div class="time">${cityTime.format(
+          "hh:mm:ss"
+        )} <small>${cityTime.format("A")}</small> </div>
+    </div>
+    <div>
+<a href="/">All Cities</a> 
+</div> 
+<style>
+a {
+    text-decoration:none;
 }
+</style>`;
+}
+
 
 updateTime();
 setInterval(updateTime, 1000);
-
 
 let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);
